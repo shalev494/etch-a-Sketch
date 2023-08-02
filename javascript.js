cfa7657026852e747ca87a0a096f6f639f7b6d8a
shalev <shalev05102008@gmail.com>
2023-08-02 13:11:55 +0300

    add buttons and random colors

--- javascript.js
+++ javascript.js
@@ -1,6 +1,8 @@
 const container = document.querySelector('.container');
 const container2 = document.querySelector('.container2');
 const resetButton = document.querySelector('button');
+resetButton.style.backgroundColor='white';
+let color="black";
 let amountOf = 4;
 const grid =(blocks)=>{
   const warapper= document.createElement('div');
@@ -10,17 +12,21 @@ const grid =(blocks)=>{
       row.classList.add('row');
   
   for (let j=0; j<blocks; j++) {
-  const gridBlock= document.createElement('div');
+  const gridBlock= document.createElement("div");
+  gridBlock.classList.add('gridblock')
   gridBlock.classList.add('gridBlock');
-  gridBlock.addEventListener('mouseenter',()=>{
-    gridBlock.style.backgroundColor= 'blue';
-  })
+  gridBlock.addEventListener('mouseover',colorDiv)
+  container.insertAdjacentElement("beforeend",gridBlock)
+    
+  
+    
   row.appendChild(gridBlock)
 }
 warapper.appendChild(row)          
 }
 container.appendChild(warapper)
 }
+
 grid(amountOf);
 resetButton.addEventListener('click',()=>{
   let inputSize=Number(prompt('Choose the size of the grid'))
@@ -32,5 +38,14 @@ resetButton.addEventListener('click',()=>{
   warapper.remove();
   grid(inputSize);
 })
-
+function colorDiv() {
+  if (currentColor === "random") {
+    this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
+  } else {
+    this.style.backgroundColor = currentColor;
+  }
+}
+function setColor(colorChoice) {
+  currentColor = colorChoice;
+}
 
